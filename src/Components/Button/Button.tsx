e7c55e87a07d3fd07adeee0bd3d73fee08561059
qhathizwe <sphoswa2@gmail.com>
2026-08-17 15:36:48 +0200

    Login css

---
+++ src/Components/Button/Button.tsx
@@ -0,0 +1,9 @@
+// import styles from './Button.module.css'
+
+// export const Button = () => {
+//   return (
+//     <div className={styles.buttonContainer}>
+//         <button className={styles.btn}></button>
+//     </div>
+//   )
+// }
--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,20 +1,26 @@
 import styles from './Login.module.css'
+import { NavLink } from 'react-router-dom';
 
 const Login = () => {
   return (
     <div className={styles.LoginContainer}>
         <div className={styles.LoginContent}>
             <h1 className={styles.title}>Please <span className={styles.tGreen}>Login</span> Your Account</h1>
-        </div>
+        
         <div className={styles.email}>
             <input type="text" placeholder='Enter Your email' />
         </div>
          <div className={styles.password}>
-            <input type="text" placeholder='Enter Your email' />
+            <input type="text" placeholder='Enter Your Password' />
         </div>
         <button className={styles.btnLogin}>Login</button>
         <div className={styles.register}>
-            <p>Don't have an account?</p>
+            <p>Don't have an account? 
+                {/* <NavLink to ="/" className={styles.register}>
+                 Register
+                </NavLink> */}
+            </p>
+        </div>
         </div>
     </div>
   )
