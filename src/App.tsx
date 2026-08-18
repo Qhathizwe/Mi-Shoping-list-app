1d7e8000075df761bbcfae8e6f5a7760a37b0e95
qhathizwe <sphoswa2@gmail.com>
2026-08-19 01:41:36 +0200

    importing nav components and importing nav to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+
 import Login from './Components/Login/Login'
 import Register from './Components/Register/Register'
+import Home from './Pages/Home'
+
 
 function App() {
   return (
@@ -10,6 +13,7 @@ function App() {
 
         <Route path="/" element={<Login />} />
         <Route path='register' element={<Register />}/>
+        <Route path='home' element={<Home />} />
       </Routes>
     </>
   )
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import Nav from "../Components/nav/Nav"
+
+const Home = () => {
+  return (
+    <div>
+      < Nav />
+    </div>
+  )
+}
+
+export default Home
