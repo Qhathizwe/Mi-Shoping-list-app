a268f472a11f0a75bc22c86b0f2c58d083a70eb1
qhathizwe <sphoswa2@gmail.com>
2026-08-18 22:37:53 +0200

    box shadow and transitions alterations

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,28 +1,28 @@
-import styles from './Login.module.css'
+import styles from './Login.module.css';
 import { NavLink } from 'react-router-dom';
 
 const Login = () => {
   return (
     <div className={styles.LoginContainer}>
-        <div className={styles.LoginContent}>
-            <h1 className={styles.title}>Please <span className={styles.tGreen}>Login</span> Your Account</h1>
+      <div className={styles.LoginContent}>
+        <h1 className={styles.title}>
+          Please <span className={styles.tGreen}>Login</span> Your Account
+        </h1>
         
-        <div className={styles.email}>
-            <input type="text" placeholder='Enter Your email' />
-        </div>
-         <div className={styles.password}>
-            <input type="password" placeholder='Enter Your Password' />
-        </div>
+        <input type="email" placeholder="Enter Your email" className={styles.loginInput} />
+        <input type="password" placeholder="Enter Your Password" className={styles.loginInput} />
+        
         <button className={styles.btnLogin}>Login</button>
-        <div className={styles.register}>
-            <p>Don't have an account? 
-                <NavLink to ="register" className={styles.Linkregister}>
-                 Register
-                </NavLink>
-            </p>
-        </div>
-        </div>
+        
+        <p className={styles.registerText}>
+          Don't have an account?{' '}
+          <NavLink to="/register" className={styles.Linkregister}>
+            Register
+          </NavLink>
+        </p>
+      </div>
     </div>
-  )
-}
-export default Login 
+  );
+};
+
+export default Login;
--- src/Components/Register/Register.tsx
+++ src/Components/Register/Register.tsx
@@ -1,33 +1,31 @@
-
-import styles from './Register.module.css'
-// import { Button } from '../Button/Button'
-import { NavLink } from 'react-router-dom'
-
+import styles from './Register.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Register = () => {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-          <h1 className={styles.title}>Please <span className={styles.titleGreen}>Register</span> Your Acoount</h1>
-        <div className={styles.nameSurname}>
-            <input type="text" className={styles.name} placeholder='Name'/>
-            <input type="text" className={styles.surname} placeholder='Surname'/>
-        </div>
-
-         
-        <div className={styles.email}><input type="email" className={styles.email} placeholder='Email'/></div>
-        <div className={styles.text}><input type="text" className={styles.Phone} placeholder='Phone Number'/></div>
-        <div className={styles.password}><input type="password" className={styles.password} placeholder='Password'/></div>
+        <h1 className={styles.title}>
+          Please <span className={styles.titleGreen}>Register</span> Your Account
+        </h1>
         
-            <button className={styles.btnRegister}>Register</button>
-
-
-            <p>Already a member? <NavLink to='/' className={styles.linkLogin} >
-            Login</NavLink>
-            </p>
+        <div className={styles.nameSurname}>
+          <input type="text" placeholder="Name" className={styles.inputHalf} />
+          <input type="text" placeholder="Surname" className={styles.inputHalf} />
+        </div>
+        
+        <input type="email" placeholder="Email" className={styles.inputFull} />
+        <input type="text" placeholder="Phone Number" className={styles.inputFull} />
+        <input type="password" placeholder="Password" className={styles.inputFull} />
+        
+        <button className={styles.btnRegister}>Register</button>
+        
+        <p className={styles.loginText}>
+          Already a member? <NavLink to="/" className={styles.linkLogin}>Login</NavLink>
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
