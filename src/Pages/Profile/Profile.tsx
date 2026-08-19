b6a96f9431254f049e1c28b71d9a6e1967cb77ab
qhathizwe <sphoswa2@gmail.com>
2026-08-19 11:49:42 +0200

    profile page alteratons

--- src/Pages/Profile/Profile.tsx
+++ src/Pages/Profile/Profile.tsx
@@ -6,26 +6,27 @@ const Profile = () => {
 
   return (
     <div className={styles.profileContainer}>
-         <div className={styles.profile}>
-            <h3 className={styles.profileTitle}>Profile</h3>
-            <img src={profile} alt="profile"  />
+         <div className={styles.profileContent}>
+             <div className={styles.profile}>
+                {/* <h3 className={styles.profileTitle}>Profile</h3> */}
+                <img src={profile} alt="profile"  />
+             </div>
+             <div className={styles.profileInputs}>
+                     <div className={styles.nameSurname}>
+              <input type="text" placeholder="Name" className={styles.inputHalf} />
+              <input type="text" placeholder="Surname" className={styles.inputHalf} />
+                     </div>
+                     
+                     <input type="email" placeholder="Email" className={styles.inputFull} />
+                     <input type="text" placeholder="Phone Number" className={styles.inputFull} />
+                     <input type="password" placeholder="Password" className={styles.inputFull} />
+             </div>
+             <div className={styles.buttons}>
+                <button className={styles.btnEdit}>Edit</button>
+                <button className={styles.btnLogOut}>Log Out</button>
+             </div>
+                 </div>
          </div>
-         <div className={styles.profileInputs}>
-        <div className={styles.nameSurname}>
-          <input type="text" placeholder="Name" className={styles.inputHalf} />
-          <input type="text" placeholder="Surname" className={styles.inputHalf} />
-        </div>
-        
-        <input type="email" placeholder="Email" className={styles.inputFull} />
-        <input type="text" placeholder="Phone Number" className={styles.inputFull} />
-        <input type="password" placeholder="Password" className={styles.inputFull} />
-         </div>
-
-         <div className={styles.buttons}>
-            <button className={styles.btnEdit}>Edit</button>
-            <button className={styles.btnLogOut}>LogOut</button> 
-         </div>
-    </div>
   )
 }
 
