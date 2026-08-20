375849031c0d2207753f51ccc977136017704fc8
scelophoswagit config --global user.email sphoswa2@gmail.comgit config --global user.name scelo <sphoswa2@gmail.com>
2026-08-20 02:46:29 +0200

    setting up login slice/reducer

--- src/Redux/Reducers/LoginSlice.ts
+++ src/Redux/Reducers/LoginSlice.ts
@@ -1,19 +1,46 @@
-// import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import {type PayloadAction } from "@reduxjs/toolkit";
 
-// interface LoginProps {
-//     email: string;
-//     password: string;
-// }
-// const initialState: LoginProps ={
-//     email: " ",
-//     password: " ",
-// }
 
-// const loginSlice = createSlice({
-//     name: 'login',
-//     initialState,
-//     reducers: {
-        
-//         }
-//     }
-// })
+interface User {
+    id: string;
+    email: string;
+    password: string;
+}
+interface AuthState {
+    user : User | null,
+    isLoading: boolean;
+    error: string | null;
+}
+const initialState: AuthState ={
+    user : null,
+    isLoading: false,
+    error: null,
+}
+
+const loginSlice = createSlice({
+    name: 'login',
+    initialState,
+    reducers: {
+        loginStart: (state) =>{
+            state.isLoading = true;
+            state.error = null;
+        },
+
+        loginSuccess: (state, action: PayloadAction<User>) =>{
+            state.isLoading = false;
+            state.user = action.payload;
+        },
+        loginFailure: (state, action: PayloadAction<string>) =>{
+            state.isLoading =false;
+            state.error = action.payload;
+        },
+        logOut: (state) =>{
+            state.user = null;
+            state.error = null;
+        }
+        }
+})
+
+export default loginSlice.reducer;
+export const {loginStart, loginSuccess, loginFailure, logOut} =loginSlice.actions;
