5c2631eec83d35a6de6af1d8d260cb8373704464
qhathizwe <sphoswa2@gmail.com>
2026-08-27 00:28:13 +0200

    importing categoryItems style into categoryItem tsx file

---
+++ src/Pages/CategoryItems/CategoryItems.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './CategoryItems.module.css'
+import CategoryItemCard from '../../Components/Catergory-Item/CategoryItemCard'
+import { useSelector } from 'react-redux'
+import type { RootState } from '../../store'
+
+
+
+const CategoryItems = () => {
+  const categoryItems = useSelector((state: RootState) => state.categoryItemsSlice)
+  return (
+    <div className={styles.CategoryItemsContainer}>
+{
+categoryItems.categoryItems.map ((categoryItem) =>(
+    <CategoryItemCard key={categoryItem.id} categoryItems={categoryItem} />
+    ))
+}
+    </div>
+  )
+}
+
+export default CategoryItems
