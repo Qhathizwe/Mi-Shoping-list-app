a35e41de89980e62c6b42e57906efff6b159a7c4
qhathizwe <sphoswa2@gmail.com>
2026-08-27 00:23:11 +0200

    category slice added functionality to fetch and post on json server

--- src/Redux/Reducers/CategorySlice.ts
+++ src/Redux/Reducers/CategorySlice.ts
@@ -1,46 +1,69 @@
 import { createSlice, createAsyncThunk, type PayloadAction } from '@reduxjs/toolkit';
 
-export interface CategoryItems {
-  id?: string;
+export interface Category{
+  id: string;
   name: string;
+  userId: string;
 }
 
 interface CategoryState {
-  form: Omit<CategoryItems, 'id'>;
+  category: Category[];
   isSuccess: boolean;
   isLoading: boolean;
   error: string | null;
 }
 
 const initialState: CategoryState = {
-  form: {
-    name: '',
-  },
+  category: [],
   isSuccess: false,
   isLoading: false,
   error: null,
 };
 
+//add thunk
+export const addCategoryThunk = createAsyncThunk(
 
-export const addCategoryThunk = createAsyncThunk<CategoryItems, Omit<CategoryItems, 'id'>>(
   'category/addCategory',
-  async (categoryData, thunkAPI) => {
+  async (categoryData: Omit<Category , "id">, thunkAPI) => {
     try {
-      const response = await fetch('http://localhost:5000/users/category', {
+  
+      const response = await fetch(`http://localhost:5000/category`, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 
+          'Content-Type': 'application/json' 
+        },
         body: JSON.stringify(categoryData),
       });
 
       if (!response.ok) {
-        throw new Error('Failed to add category on server');
+        throw new Error('Failed to add category to this user on server');
       }
 
       const data = await response.json();
-      return data as CategoryItems;
+      return data;
     } catch (error: any) {
       return thunkAPI.rejectWithValue(error.message || 'Something went wrong');
     }
+  }
+);
+
+//fetching category DATA THUNK
+export const getCategoryThunk = createAsyncThunk(
+  'category/getCategoryThunk', 
+  async () => {
+    const response = await fetch('http://localhost:5000/category', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    
+    if (!response.ok) {
+      throw new Error('Failed to fetch category');
+    }
+    
+    const data = await response.json();
+    return data;
   }
 );
 
@@ -49,16 +72,13 @@ const categorySlice = createSlice({
   initialState,
   reducers: {
    
-    updateCategoryForm: (
-      state,
-      action: PayloadAction<{ field: keyof CategoryState['form']; value: string }>
-    ) => {
-      state.form[action.payload.field] = action.payload.value;
-    },
-    resetForm: (state) => {
-      state.form.name = '';
-      state.isSuccess = false;
-    }
+    // updateCategoryForm: (state,action: PayloadAction<string> ) => {
+    //   state. = action.payload;
+    // },
+    // resetForm: (state) => {
+    //   state.form.name = '';
+    //   state.isSuccess = false;
+    // }
   },
   extraReducers: (builder) => {
     builder
@@ -66,17 +86,21 @@ const categorySlice = createSlice({
         state.isLoading = true;
         state.error = null;
       })
-      .addCase(addCategoryThunk.fulfilled, (state) => {
+      .addCase(addCategoryThunk.fulfilled, (state, action: PayloadAction<Category>) => {
         state.isLoading = false;
         state.isSuccess = true;
-        state.form.name = ''; 
+        state.category.push(action.payload)
       })
       .addCase(addCategoryThunk.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload as string;
       });
+    builder.addCase(getCategoryThunk.fulfilled, (state, action) =>{
+      state.isLoading =false;
+      state.category = action.payload;
+    });
   }
 });
 
-export const { updateCategoryForm, resetForm } = categorySlice.actions;
+export const {  } = categorySlice.actions;
 export default categorySlice.reducer;
