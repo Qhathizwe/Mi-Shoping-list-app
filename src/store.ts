a76bc00b2bf8eb767807f1a08adcd557da6aa246
qhathizwe <sphoswa2@gmail.com>
2026-08-28 11:54:16 +0200

    adding categoryItems slice and Profile slice oon the store

--- src/store.ts
+++ src/store.ts
@@ -1,30 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { useDispatch } from 'react-redux'
-import { useSelector } from 'react-redux'
-import {type TypedUseSelectorHook } from 'react-redux'
-
-import loginSlice from './Redux/Reducers/LoginSlice'
-import registerSlice from './Redux/Reducers/RegisterSlice'
-import categorySlice from './Redux/Reducers/CategorySlice'
-import categoryItemsSlice from './Redux/Reducers/CategoryItemsSlice'
-// import profileSlice from './Redux/Reducers/ProfileSlice'
-
-
+import { configureStore } from '@reduxjs/toolkit';
+import loginSlice from './Redux/Reducers/LoginSlice';
+import registerSlice from './Redux/Reducers/RegisterSlice';
+import categorySlice from './Redux/Reducers/CategorySlice';
+import categoryItemsSlice from './Redux/Reducers/CategoryItemsSlice';
+import profileSlice from './Redux/Reducers/ProfileSlice';
+import { useDispatch, useSelector, type TypedUseSelectorHook } from 'react-redux'; 
 
 export const store = configureStore({
   reducer: {
       auth: loginSlice,
       register: registerSlice,
-      // profile : profileSlice,
-      category : categorySlice,
+      category: categorySlice,
       categoryItemsSlice: categoryItemsSlice,
+      profile: profileSlice, // 2. Reactivate key mapping
   },
-})
+});
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
