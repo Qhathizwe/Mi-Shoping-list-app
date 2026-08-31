e8ea2626449ba9b87414f8c0ff7742b7e610e8ca
qhathizwe <sphoswa2@gmail.com>
2026-08-31 08:45:29 +0200

    search (search bar and button) component and styles

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,14 +1,26 @@
-import styles from './Search.module.css'
+import React from 'react';
+import styles from './Search.module.css';
 
-const Search = () => {
+interface SearchProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Search: React.FC<SearchProps> = ({ value, onChange }) => {
   return (
     <div className={styles.searchContainer}>
       <div className={styles.searchContent}>
-      <input type="text" placeholder='Search here' className={styles.searchInput} />
-      <button className={styles.btnSearch}>Search</button>
+        <input 
+          type="text" 
+          placeholder='Search items by name...' 
+          className={styles.searchInput} 
+          value={value}
+          onChange={onChange}
+        />
+        <button type="button" className={styles.btnSearch}>Search</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
