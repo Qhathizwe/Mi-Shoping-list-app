f4c2c97233e2a9fb46fa2d290945c8dd2af6a18d
qhathizwe <sphoswa2@gmail.com>
2026-08-31 14:58:13 +0200

    protected  routes tsx file

---
+++ src/Components/Protected Routes/ProtectedRoutes.tsx
@@ -0,0 +1,11 @@
+// import type{JSX} from 'react'
+// import { Outlet, Navigate, useLocation } from 'react-router-dom'
+
+// export interface LocationState {
+//   from: {
+//     pathname: string;
+//   };
+// }
+
+// const ProtectedRoute = (): JSX.Element => {
+//   const location = useLocation();
