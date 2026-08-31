c39e6772e98bf49a61068d8a895b9f637745cb0b
qhathizwe <sphoswa2@gmail.com>
2026-08-31 08:42:23 +0200

    adding landing page to the routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,15 @@ import Home from './Pages/Home.1/Home'
 import  Profile  from './Pages/Profile/Profile'
 import CategoryItems from './Pages/CategoryItems/CategoryItems'
 
+import LandingPage from './Pages/Landing Page/LandingPage'
+
 
 function App() {
   return (
     <>
       <Routes>
-
-        <Route path="/" element={<Login />} />
+        <Route path='/' element={< LandingPage />}/>
+        <Route path="/login" element={<Login />} />
         <Route path='/register' element={<Register />}/>
         <Route path='/home' element={<Home />} />
         <Route path='/profile' element={<Profile />} />
