2344ccf6285c8a941a6a028ef39d32e6732dcb2a
qhathizwe <sphoswa2@gmail.com>
2026-08-31 08:52:23 +0200

    category items components alterations with button setup on the card

--- src/Components/Catergory-Item/CategoryItemCard.tsx
+++ src/Components/Catergory-Item/CategoryItemCard.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
-import styles from './CategoryItems.module.css'
-import {type CategoryItems } from '../../Redux/Reducers/CategoryItemsSlice';
+import styles from './CategoryItems.module.css';
+import { type ListItem, deleteCategoryItemThunk, setEditingItem } from '../../Redux/Reducers/CategoryItemsSlice';
+import { useAppDispatch } from '../../store';
 
-interface CategoryItemsProp{
-    categoryItems: CategoryItems;
-    id?: string; 
+interface CategoryItemsProp {
+  categoryItems: ListItem;
+  onOpenModal: () => void; 
 }
 
-const CategoryItemCard: React.FC<CategoryItemsProp> = ({categoryItems}) => {
+const CategoryItemCard: React.FC<CategoryItemsProp> = ({ categoryItems, onOpenModal }) => {
+  const dispatch = useAppDispatch();
+
+  const handleEditClick = () => {
+    dispatch(setEditingItem(categoryItems)); // Hydrate form variables into global state
+    onOpenModal();
+  };
+
+  const handleDeleteClick = () => {
+    if (window.confirm(`Are you sure you want to delete ${categoryItems.name}?`)) {
+      dispatch(deleteCategoryItemThunk(categoryItems.id));
+    }
+  };
 
   return (
-    <div>
-      <h2>{categoryItems.name}</h2>
-       <input type="number"  />{categoryItems.quantity}
-      <h4>{categoryItems.notes}</h4>
+    <div className={styles.itemCard}>
+      <div className={styles.itemCardLeft}>
+        {categoryItems.picture && (
+          <div className={styles.imageWrapper}>
+            <img src={categoryItems.picture} alt={categoryItems.name} className={styles.itemImage} />
+          </div>
+        )}
+        <div className={styles.itemDetails}>
+          <h2 className={styles.itemName}>{categoryItems.name}</h2>
+          <span className={styles.itemQuantity}>Qty: {categoryItems.quantity}</span>
+          {categoryItems.notes && <p className={styles.itemNotes}>{categoryItems.notes}</p>}
+        </div>
+      </div>
       
-       <button className={styles.btnEdit} >Edit</button>
-       <button className={styles.btnDelete} >Delete</button>
+      <div className={styles.itemActions}>
+        <button className={styles.btnCardEdit} onClick={handleEditClick}>Edit</button>
+        <button className={styles.btnCardDelete} onClick={handleDeleteClick}>Delete</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default CategoryItemCard;
