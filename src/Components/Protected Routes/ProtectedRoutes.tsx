a6c98c336f005627e0a6f508af41f8595b9873a7
qhathizwe <sphoswa2@gmail.com>
2026-09-01 10:52:12 +0200

    protected routes tsx file to protect my routes

--- src/Components/Protected Routes/ProtectedRoutes.tsx
+++ src/Components/Protected Routes/ProtectedRoutes.tsx
@@ -1,11 +1,7 @@
-// import type{JSX} from 'react'
-// import { Outlet, Navigate, useLocation } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom';
 
-// export interface LocationState {
-//   from: {
-//     pathname: string;
-//   };
-// }
-
-// const ProtectedRoute = (): JSX.Element => {
-//   const location = useLocation();
+export const ProtectedRoutes = ({ isAuthenticated }: { isAuthenticated: boolean }) => 
+    {
+  return isAuthenticated ? <Outlet />
+   : <Navigate to="/login" replace />;
+};
