73cce624d8b10bfe39c59b6a3cf9a14ba4e1f4e0
qhathizwe <sphoswa2@gmail.com>
2026-09-01 09:15:32 +0200

    importing login component to login page

---
+++ src/Pages/register page/RegisterPage.tsx
@@ -0,0 +1,10 @@
+import RegisterPage from "../../Components/Register/Register"
+
+export const Register = () => {
+  return (
+    <div>
+        <RegisterPage />
+    </div>
+  )
+}
+export default Register
